403ad599482b360cd2b9e7bdc9ac4c255d2ccf19
deweiye <de-wei.ye@houzz.com>
2021-08-23 18:35:49 +0800

    addTodo component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import  { useSelector, useDispatch } from 'react-redux'
-import { selectTodos } from './redux/selectors'
 import './App.css';
-import { addTodo } from './redux/actions';
+import AddTodo from './addTodo';
+
 
 function App() {
-  const todos = useSelector(selectTodos)
-  const dispatch = useDispatch()
-  console.log("state:", todos)
   return (
     <div>
-      <button onClick={() => {
-        dispatch(addTodo(Math.random()))
-      }}>Add Todo</button>
-      <ul>
-        {
-          todos.map((todo) => {
-            return <li>{todos.id} {todo.name}</li>
-          })
-        }
-      </ul>
+      <AddTodo />
     </div>
   );
 }
